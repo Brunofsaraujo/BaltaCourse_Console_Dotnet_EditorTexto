b7a2e06cffd7abaa7a0a4dfa1b5ca8166cd887a5
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-01-09 22:09:36 -0300

    EditorTexto: Criação projeto

---
+++ BaltaCourse_Console_Dotnet_EditorTexto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaltaCourse_Console_Dotnet_EditorTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
